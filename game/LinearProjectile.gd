a8c7ca64a63a0fa41ccd3d217f0d29d6d30a807d
qwertychomp <supersequoia11@gmail.com>
2023-12-01 18:40:11 -0800

    better projectile and story draft

--- game/LinearProjectile.gd
+++ game/LinearProjectile.gd
@@ -1,5 +1,5 @@
 extends Area2D
-
+#TODO: f
 @onready var hitbox = get_node("hitbox")
 @onready var sprite = get_node("Sprite2D")
 @onready var isInLib = (self.get_parent()!=get_node("/root/mainScene/commonObjLibrary"))
@@ -7,9 +7,13 @@ extends Area2D
 @onready var fireNow
 @onready var despawnTime#this is in ms. after the projectile has bounced the amount equal to whatever bounceamount is, it will stay still and sit until despawmtime is up.
 #pos is a vector2
+@onready var firetime
+@onready var despawnCounter = 0
+@onready var hitamount = 0
+@onready var hitlimit = 0
 #the NotSliding variable prevents the bounce efect being initiated multiple times because the projectile doesnt escape the hitbox it hit on the frame in which it happened
-@onready var NotSliding = false
-func fire(angle,pos,speedIn,despawnTime,bounceAmount):
+@onready var NotSliding = true
+func fire(angle,pos,speedIn,despawnpime,bounceAmount):
 	#set hitbox size to sprite size
 	hitbox.shape.extents = sprite.get_rect().size/2
 	self.position.x = pos.x
@@ -18,10 +22,12 @@ func fire(angle,pos,speedIn,despawnTime,bounceAmount):
 	speed = speedIn
 	self.visible = true
 	fireNow = true
-	
-	
+	hitlimit = bounceAmount
+	despawnTime = despawnpime
+#func get_collision_angle():
 func _ready():
-	fire(-100,Vector2(0,0),200,999,999)
+	#despawn time is in seconds, i think?
+	fire(40,Vector2(0,0),500,6,1)
 	if(!isInLib):
 		self.visible = false
 		self.get_node("hitbox").set_deferred("disabled", true)
@@ -29,21 +35,30 @@ func _ready():
 func _process(delta):
 	if(isInLib):
 		if (fireNow):
-			#this shit had better work
-			if(NotSliding):
-				if(self.has_overlapping_areas() or self.has_overlapping_bodies()):
-					self.rotation_degrees = -self.rotation_degrees
-					
-					#print(NotSliding)
-					NotSliding = false
-			self.position.x += speed*delta
-			#print(self.rotation_degrees)
-			self.position.y += (speed*delta)*tan(self.rotation_degrees)
-			if(!self.has_overlapping_areas() or self.has_overlapping_bodies()):
-				#print("e")
-				NotSliding = true
-			#self.hitbox.position.x = self.position.x
-			#self.hitbox.position.y = self.position.y
-			#print(self.position.x)
-			#print(self.hitbox.position.x)
-		
+			if(!hitamount==hitlimit or hitamount==0):
+				print("ec")
+				if(NotSliding):
+					if(self.has_overlapping_areas() or self.has_overlapping_bodies()):
+						print(self.position.x)
+						print(self.position.y)
+						print("thingy got hit!")
+						hitamount = hitamount+1
+						self.rotation_degrees = -self.rotation_degrees
+						#speed = -speed
+						NotSliding = false
+				print("reached")
+				self.position.x += speed*delta
+				self.position.y += tan(deg_to_rad(self.rotation_degrees))*speed*delta
+				#debug code
+				if(!NotSliding):
+					print(self.position.x)
+					print(self.position.y)
+				if(!self.has_overlapping_areas() or self.has_overlapping_bodies()):
+					NotSliding = true		
+				firetime = Time.get_unix_time_from_system()
+			else:
+					despawnCounter = Time.get_unix_time_from_system()-firetime
+					print(despawnTime)
+					print(despawnCounter)
+					if(despawnCounter>=despawnTime):
+						self.queue_free()
